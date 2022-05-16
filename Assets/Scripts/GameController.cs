b8df2ad88fde66c83ffce225d12ea8508046bfbc
Natalia Silva <git.qodeless@outlok.com>
2022-05-16 20:51:37 -0300

    Jogo base pronto

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField]private GameObject PalavraGrid, EspacoGrid;
-    [SerializeField]private GameObject LetraPrefab, EspacoPrefab;
-    [SerializeField]private InputField CampoInputField;
-    [SerializeField]private Button TryButton;
-    [SerializeField]string Palavra;
+    const int ACERTO_POR_LETRA = 5;
+    const int ACERTO_PALAVRA = 100;
+    [SerializeField] private GameObject PalavraGrid, EspacoGrid;
+    [SerializeField] private GameObject LetraPrefab, EspacoPrefab;
+    [SerializeField] private GameObject CaixaTextoLetrasEliminadas;
+    [SerializeField] private GameObject CaixaTextoPontuacao;
+    [SerializeField] private GameObject Stickerman;
+    [SerializeField] private InputField CampoInputField;
+    [SerializeField] private Button TryButton;
+    private string Palavra;
+    private string LetrasEliminadas;
+    private int Vida;
+    private int PontuacaoAtual;
+    private int QtdAcertos;
+
     #region Singleton
     //Singleton
     public static GameController Instance { get; protected set; }
@@ -22,9 +35,13 @@ public class GameController : MonoBehaviour
     }
     #endregion
 
-    private void Start()
+    private void Start()//Inicializa a rodada
     {
-        Instance.Palavra = ("sucesso").ToUpper();  //Puxar do database
+        Instance.Vida = 6;
+        Instance.LetrasEliminadas = "";
+        Instance.PontuacaoAtual = 0; //Puxar do database
+        Instance.QtdAcertos = 0;
+        Instance.Palavra = ("pao").ToUpper();  //Puxar do database
         GenerateGrid();
         TryButton.onClick.AddListener(TryLetra);
     }
@@ -45,16 +62,85 @@ public class GameController : MonoBehaviour
 
     public void TryLetra()
     {
+        if (string.IsNullOrEmpty(CampoInputField.text))
+            return;
+
         string palavra = Instance.Palavra;
         char letraDigitada = CampoInputField.text.ToUpper().ToCharArray()[0];//TODO: acentuação
+        bool acertou = false;
+
+        if (VerificaLetraRepetida(letraDigitada))//Verifica letra repetida e limpa
+            return;
 
         for (int i = 0; i < palavra.Length; i++)
         {
             if (palavra[i] == letraDigitada)
             {
                 PalavraGrid.transform.GetChild(i).gameObject.GetComponent<Text>().enabled = true;
+                Instance.QtdAcertos++;
+                acertou = true;
             }
         }
-        CampoInputField.text = string.Empty;
+        Instance.LetrasEliminadas += letraDigitada; //Adiciona a letra na lista de tentativas
+        Instance.CaixaTextoLetrasEliminadas.GetComponent<Text>().text = Instance.LetrasEliminadas;// TODO: REGEX
+        LifeUpdate(acertou);
+    }
+    #region REGEX_ANDAMENTO
+    //public string useRegex(String input)
+    //{// Console.WriteLine("Returned string: " + Regex.Replace(uncPath, pattern, replacement, RegexOptions.IgnoreCase));
+
+    //    var itensSeparados = texto.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries)
+    //.Select(x => x.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries))
+    //.ToList();
+    //var testRegex = Regex.Replace(Instance.LetrasEliminadas, @"[^A-Z]", "-");
+    //    Regex regexPattern = new Regex("([a-zA-Z](-[a-zA-Z])+)", RegexOptions.IgnoreCase);
+    //    string pattern = @"([a-zA-Z](-[a-zA-Z])+)";
+    //    string replacement = "$1";
+    //    string input = "$16.32 12.19 £16.29 €18.29  €18,29";
+    //    string result = Regex.Replace(input, pattern, replacement);
+    //    Console.WriteLine(result);
+
+    //    return result;
+    //}
+    #endregion
+    public bool VerificaLetraRepetida(char letraDigitada) //Se a letra já foi digitada retorna true
+    {
+        Instance.CaixaTextoLetrasEliminadas.GetComponent<Text>().text = Instance.LetrasEliminadas;
+        CampoInputField.text = string.Empty;//Limpa o campo onde o usuário digitada
+        foreach (var letra in Instance.LetrasEliminadas)
+        {
+            if (letra == letraDigitada)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void LifeUpdate(bool acertou)
+    {
+        if (acertou)
+        {
+            Instance.PontuacaoAtual += ACERTO_POR_LETRA;
+            Instance.CaixaTextoPontuacao.GetComponent<TextMeshProUGUI>().text = Instance.PontuacaoAtual.ToString();
+
+            if (Instance.QtdAcertos == Instance.Palavra.Length)//Verifica vitoria
+            {
+                Instance.PontuacaoAtual += ACERTO_PALAVRA;
+                Instance.CaixaTextoPontuacao.GetComponent<TextMeshProUGUI>().text = Instance.PontuacaoAtual.ToString();
+                Debug.Log("VITORIA");
+                //TODO: TELA REINICIA
+            }
+            return;
+        }
+
+        Instance.Vida--;
+        Instance.Stickerman.transform.GetChild(Instance.Vida).gameObject.SetActive(false);
+        if (Instance.Vida == 0)
+        {
+            Debug.Log("GAME OVER");
+            Instance.Stickerman.GetComponent<Image>().color = Color.red;
+            return;
+            //TODO: TELA REINICIA
+        }
     }
 }
